6f5b7361878a09b1b80cc9943165783d457ceb9c
NYC_Anindit <143905382+NYCAnindit@users.noreply.github.com>
2024-07-29 23:43:31 +0530

    Time: 2 ms (43.96%), Space: 10.2 MB (5.64%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,31 +1,18 @@
+#include <vector>
+#include <algorithm>
+
 class Solution {
-    
 public:
-    void sortColors(vector<int>& nums) {
-        int l =0;
-        int n =nums.size();
-        int h=n-1;
-        int m=0;
-        while(m<=h)
-        {
-            int x =nums[m];
-            if(x==0)
-            {
-                swap(nums[l],nums[m]);
-                l++;
-                m++;
-                    
-            }
-            else if(x==1)
-            {
-               m++;
-            }
-            else{
-                swap(nums[m],nums[h]);
-                h--;
+    void sortColors(std::vector<int>& nums) {
+        int n = nums.size();
+        
+        // Bubble sort approach
+        for (int i = 0; i < n - 1; ++i) {
+            for (int j = 0; j < n - i - 1; ++j) {
+                if (nums[j] > nums[j + 1]) {
+                    std::swap(nums[j], nums[j + 1]);
+                }
             }
         }
-
-        
     }
 };

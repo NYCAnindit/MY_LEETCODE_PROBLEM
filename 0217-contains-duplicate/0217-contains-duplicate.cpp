07508f467c70b6c768c8a772102b7c62cba931b7
NYC_Anindit <143905382+NYCAnindit@users.noreply.github.com>
2024-10-08 21:30:59 +0530

    Time: 125 ms (15.24%), Space: 76.2 MB (5.65%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,8 +3,8 @@ public:
     bool containsDuplicate(vector<int>& nums) {
        set<int>s(nums.begin(),nums.end());
         int n =nums.size();
-        int y=s.size();
-        if(n==y)
+        int y =s.size();
+        if(y==n)
         {
             return false;
         }

195b9b686e8a0b83d48dcb242fb1e0c260937010
NYC_Anindit <143905382+NYCAnindit@users.noreply.github.com>
2024-10-09 22:00:04 +0530

    Time: 0 ms (100.00%), Space: 16.9 MB (27.97%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    // Helper function to calculate the minimum cost to reach the top
-    int ways(int n, vector<int>& cost, vector<int>& dp) {
-        // Base cases
-        if (n <= 1) return cost[n];
-        
-        // If already calculated, return the stored value
-        if (dp[n] != -1) return dp[n];
-        
-        // Recursive case: take the minimum of the two previous steps
-        dp[n] = cost[n] + min(ways(n - 1, cost, dp), ways(n - 2, cost, dp));
-        
-        return dp[n];
+    int ways(int n,vector<int>& cost,vector<int>&dp)
+    {
+        if(n<=1)
+        {
+            return 0;
+        }
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+    
+    return dp[n] = min(cost[n-1]+ways(n-1,cost,dp),(cost[n-2]+ways(n-2,cost,dp)));
     }
-
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> dp(n + 1, -1); // DP array for memoization
+        int n =cost.size();
+        vector<int>dp(n+1,-1);
+        return ways(n,cost,dp);
         
-        // Since you can start from either the last or second-last step, take the minimum of the two
-        return min(ways(n - 1, cost, dp), ways(n - 2, cost, dp));
+        
     }
 };
